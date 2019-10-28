20a2be9bc8a63ad19822676687bb190c5b21a6dc
EulerChavez <eulerchavez@outlook.com>
2019-10-28 14:37:41 -0600

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -26,15 +26,49 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private alertCtrl: AlertController,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      const subscription = this.platform.backButton.subscribeWithPriority(999990, async () => {
+
+        const alert = await this.alertCtrl.create({
+          message: '¿Deseas salir de la aplicación?', buttons: [
+            {
+              text: 'Cancelar',
+              handler: async () => {
+              }
+            },
+            {
+              text: 'Aceptar',
+              handler: async () => {
+
+                if (subscription) { subscription.unsubscribe(); }
+
+                navigator['app'].exitApp();
+              },
+            }
+          ]
+        });
+
+        if (this.router.url === '/home') {
+
+          alert.present();
+
+        } else {
+
+          window.history.back();
+        }
+      });
     });
   }
 }
